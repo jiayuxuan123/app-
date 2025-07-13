1e58beb5d6c9007ed54e7553676c8d07a058b14b
jiayuxuan123 <jiayuxuan_2024@qq.com>
2025-07-13 11:13:15 +0800

    Update data.js

--- data.js
+++ data.js
@@ -4,9 +4,9 @@
 export const allAppsData = [
   {
     "id": "app-hub-starter",
-    "name": "App Hub_作者：jiayuxuan 123",
+    "name": "App Hub",
     "type": "纯前端",
-    "tagline": "一个基于 HTML、CSS 和 JS 的应用商店",
+    "tagline": "jiayuxuan 123",
     "description": "这个应用商店本身就是一个纯前端应用，旨在收录和展示各种类型的酷炫应用。它使用模块化 JavaScript 和 CSS 变量构建，响应式设计，支持快速搜索和过滤。您可以基于此项目来构建自己的应用商店。",
     "logo_url": "https://raw.githubusercontent.com/jiayuxuan123/app-/main/assets/images/app-hub-logo.png",
     "screenshot_url": "https://raw.githubusercontent.com/jiayuxuan123/app-/main/assets/images/app-hub-screenshot.png",
@@ -16,10 +16,10 @@ export const allAppsData = [
     "date_added": "2025-07-13"
   },
   {
-    "id": "2048-game_作者：gabrielecirulli",
+    "id": "2048-game",
     "name": "2048",
     "type": "纯前端",
-    "tagline": "经典的 2048 游戏，纯前端实现",
+    "tagline": "gabrielecirulli",
     "description": "一个经典的 2048 游戏克隆版，完全使用 HTML、CSS 和 JavaScript 构建，无需任何后端。界面简洁，支持键盘和滑动操作，游戏体验流畅。",
     "logo_url": "https://raw.githubusercontent.com/gabrielecirulli/2048/master/style/img/2048_logo.png",
     "screenshot_url": "https://raw.githubusercontent.com/gabrielecirulli/2048/master/meta/2048.png",
